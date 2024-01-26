25403e443c8784d9a60739037c3f3853e89e125f
Iryna Burlaka <i.am.blinova@gmail.com>
2024-01-26 12:20:59 +0100

    and thes one

--- src/index.js
+++ src/index.js
@@ -3,18 +3,18 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'App';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
-import { store, persistor } from 'store';
+import { store, persistor } from './Redux/store';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="Watertracker">
-          <App />
-        </BrowserRouter>
-      </PersistGate>
+      {/* <PersistGate loading={null} persistor={persistor}>*/}
+      <BrowserRouter basename="Watertracker">
+        <App />
+      </BrowserRouter>
+      {/* </PersistGate>*/}
     </Provider>
   </React.StrictMode>
 );
