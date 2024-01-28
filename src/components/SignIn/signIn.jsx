b2a5f1be922a58fca6c30add9bd4fd12760c44c3
ndnastia <ndnastiadatsiuk@gmail.com>
2024-01-28 19:57:25 +0200

    sign up page + validation for sign in page

--- src/components/SignIn/signIn.jsx
+++ src/components/SignIn/signIn.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { AuthStyledForm, AuthDiv, FormName, InputDiv, StyledInput, FormButton } from './signIn.styled';
+import { AuthStyledForm, AuthDiv, FormName, InputDiv, StyledInput, FormButton, AuthDataError } from './signIn.styled';
 import { useFormik } from 'formik';
 import { loginThunk } from '../../Redux/auth/authOperations';
 import { useDispatch } from 'react-redux';
+import * as Yup from 'yup';
+
+const validationSchema = Yup.object({
+  email: Yup
+    .string('Enter your email')
+    .email('Enter a valid email')
+    .required('Email is required'),
+  password: Yup
+    .string('Enter your password')
+    .min(6, 'Password should be of minimum 6 characters length')
+    .required('Password is required'),
+
+});
 
 
 
- 
  export const AuthForm = () => {
 
  const dispatch = useDispatch();
@@ -16,7 +28,7 @@ import { useDispatch } from 'react-redux';
        email: '',
        password: '', 
       },
-      
+      validationSchema: validationSchema,
       onSubmit: values => {
         dispatch(loginThunk(values));
         formik.resetForm();
@@ -39,11 +51,17 @@ import { useDispatch } from 'react-redux';
             onChange={formik.handleChange}
             value={formik.values.email}
             placeholder="E-mail"
+            className={
+              formik.errors.email && formik.touched.email
+                ? 'error'
+                : ''
+            }
           />
-        </InputDiv>
+         {formik.errors.email && formik.touched.email ? <AuthDataError>{formik.errors.email}</AuthDataError> : null} 
+          </InputDiv>
        
        <InputDiv>
-            <label htmlFor="email">Enter your password</label>
+            <label htmlFor="password">Enter your password</label>
              <StyledInput
                 id="password"
                 name="password"
@@ -51,7 +69,16 @@ import { useDispatch } from 'react-redux';
                 onChange={formik.handleChange}
                 value={formik.values.password}
                 placeholder='Password'
+                className={
+                  formik.errors.password && formik.touched.password
+                    ? 'error'
+                    : ''
+                }
             />
+
+            {formik.errors.password && formik.touched.password ? (
+            <AuthDataError>{formik.errors.password}</AuthDataError>
+          ) : null}
     
        </InputDiv>
        
