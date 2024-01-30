951e523a23eabf13ae81c73bee96acbd781cdf79
Michael <fidd.michael@gmail.com>
2024-01-30 13:49:39 +0100

    resolve conflicts

---
+++ src/components/DailyNorma/DailyNorma.styled.js
@@ -0,0 +1,15 @@
+const { default: styled } = require("styled-components");
+
+
+export const StyledDalyNorma = styled.div`
+    
+    padding: 8px 20px;
+    border-radius: 10px;
+    border: 1px solid var(--secondary-color-2);
+    width: fit-content;
+
+    .title{
+        font-size: 18px;
+        line-height: 1.3;
+    }
+`
