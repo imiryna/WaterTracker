94235e963f9e00e850dbb9afb8288da77de6fde2
Michael <fidd.michael@gmail.com>
2024-01-30 13:41:50 +0100

    deleted redux user directory, update logo icon

--- src/Redux/users/usersSelectors.js
+++
@@ -1,5 +0,0 @@
-// export const selectUsers = state => state.users.items;
-// export const selectIsLoading = state => state.users.isLoading;
-// export const selectError = state => state.users.error;
-
-
