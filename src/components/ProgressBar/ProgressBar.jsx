eb52147a07084b41b61a820a738df5e797f95908
Serhii01071994 <126762718+Serhii01071994@users.noreply.github.com>
2024-01-30 14:30:02 +0200

    стили готовы

--- src/components/ProgressBar/ProgressBar.jsx
+++ src/components/ProgressBar/ProgressBar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { ProgressBarStyled } from './ProgressBar.styled';
+import { ReactComponent as Icon } from './progressBar-icon.svg';
 
 export const ProgressBar = () => {
   const [progress, setProgress] = useState(0);
@@ -19,32 +20,36 @@ export const ProgressBar = () => {
 
   return (
     <ProgressBarStyled>
-      <h2 className='progress-bar-title'>Today</h2>
-      <div className="progress-bar">
-        <div
-          className="progress-bar-fill"
-          style={{ width: `${progress}%`, backgroundColor: getColor() }}
-        >
-          <div className="moving-dot" style={{ left: `${progress}%` }}></div>
+      <div className="progress-bar-block">
+        <h2 className="progress-bar-title">Today</h2>
+        <div className="progress-bar">
+          <div
+            className="progress-bar-fill"
+            style={{ width: `${progress}%`, backgroundColor: getColor() }}
+          >
+            <div className="moving-dot" style={{ left: `${progress}%` }}></div>
+          </div>
         </div>
+        <ul className="progress-bar-lines">
+          <li className="progress-bar-line"></li>
+          <li className="progress-bar-line"></li>
+          <li className="progress-bar-line"></li>
+        </ul>
+        <ul className="progress-bar-labels">
+          <li className="progress-bar-label-0 progress-bar-label">0%</li>
+          <li className="progress-bar-label-50 progress-bar-label">50%</li>
+          <li className="progress-bar-label-100 progress-bar-label">100%</li>
+        </ul>
       </div>
-      <ul className='progress-bar-labels'>
-        <li>
-          <div className="progress-bar-lines"></div>
-          <div className="progress-bar-label">{progress}%</div>
-        </li>
-        <li>
-          <div className="progress-bar-lines"></div>
-          <div className="progress-bar-label">{progress}%</div>
-        </li>
-        <li>
-          <div className="progress-bar-lines"></div>
-          <div className="progress-bar-label">{progress}%</div>
-        </li>
-      </ul>
-
-      <button onClick={handleButtonClick}>Progress</button>
-      <button onClick={handleButtonReset}>Reset</button>
+      <div className="button-block">
+        <button onClick={handleButtonClick} className="progress-bar-button">
+          <span className="svg-icon">
+            <Icon />
+          </span>
+          <span className="button-text">Add water</span>
+        </button>
+        {/* <button onClick={handleButtonReset}>Reset</button> */}
+      </div>
     </ProgressBarStyled>
   );
 };
