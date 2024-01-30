92d47818a8363c2ee3bc8c72b3744f2666080664
Serhii01071994 <126762718+Serhii01071994@users.noreply.github.com>
2024-01-30 20:14:29 +0200

    добавление компонента в HomePage, добавление шрифтов через переменную

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,13 +4,15 @@ import { SectionCss } from '../components/Advicer/Advicer.styled';
 import { TodayList } from 'components/TodayWaterList/TodayList';
 import { DailyNorma } from 'components/DailyNorma/DailyNorma';
 import { Calendar } from 'components/Calendar/Calendar';
+import { ProgressBar } from 'components/ProgressBar/ProgressBar';
 
 
 const HomePage = () => {
   return <SectionCss>Thes is a Home page &#128540;
     <TodayList/>
     <Calendar/>
-    <DailyNorma/>
+    <DailyNorma />
+    <ProgressBar/>
   </SectionCss>;
 
 };
