c6ab085ad5e276fd5ee512476bf2433970a60e9f
Vlad Zubin <zoobeen1@gmail.com>
2024-02-02 14:37:43 +0200

    load avatar from base in form

--- src/components/SettingModal/SettingModal.jsx
+++ src/components/SettingModal/SettingModal.jsx
@@ -1,5 +1,10 @@
 import { UserForm } from './UserForm/UserForm';
-import { toggleSettingsVisibility } from '../../Store/modals/modalSlice';
+import { toggleSettingsVisibility } from 'Store/modals/modalSlice';
+
+// temp - user from auth - it is wrong way
+import { selectAuthUserData } from 'Store/auth/authSelector';
+// import { selectUser } from 'Store/currentUser/currentUserSelectors';
+
 import { useDispatch, useSelector } from 'react-redux';
 import {
   StyledHeader,
@@ -17,15 +22,21 @@ import {
 export const Setting = imgData => {
   const dispatch = useDispatch();
   const toggleModal = () => dispatch(toggleSettingsVisibility());
-  // tmp image data
-  // todo - картинка из базы
-  const { imgUrl, imgName } = {
-    imgUrl:
-      'https://img.freepik.com/premium-vector/bearded-man-avatar-man-vector-portrait_9385-36.jpg?w=740',
-    imgName: 'Morda lica',
-  };
+  // temp - user from auth - it is wrong way
+  const user = useSelector(selectAuthUserData);
+  // const user = useSelector(selectUser);
+  console.log(user);
 
-  // const { imgUrl, imgName } = imgData;
+  const { imgUrl, imgName } = {
+    imgUrl: user.avatarUrl,
+    imgName: `Avatar for ${user.name}`,
+  };
+  // const { imgUrl, imgName } = {
+  //   imgUrl:
+  //     'https://img.freepik.com/premium-vector/bearded-man-avatar-man-vector-portrait_9385-36.jpg?w=740',
+  //   imgName: 'Morda lica',
+  // };
+
   return (
     <StyledContainer>
       <StyledHeader>
