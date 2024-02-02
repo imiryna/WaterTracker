5782fcda6e96674ffa41d425c370df16dde01e71
ndnastia <ndnastiadatsiuk@gmail.com>
2024-02-02 21:47:03 +0200

    log out

--- src/components/LogOutModal/LogOutModal.jsx
+++ src/components/LogOutModal/LogOutModal.jsx
@@ -1,11 +1,15 @@
+import { GlobalConfirmDialogStyles } from './LogOutModal.styled';
 import React from 'react';
+import { Navigate } from 'react-router-dom';
 import { ConfirmDialog } from 'primereact/confirmdialog';
 import { logOutThunk } from 'Store/auth/authOperations';
-import { useDispatch } from 'react-redux';
-import { GlobalConfirmDialogStyles } from './LogOutModal.styled';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { selectAuthAuthenticated } from 'Store/auth/authSelector';
 
 const LogoutConfirmationDialog = ({ visible, onHide }) => {
   const dispatch = useDispatch();
+  const isAuthenticated = useSelector(selectAuthAuthenticated);
 
   const onConfirm = () => {
     dispatch(logOutThunk());
@@ -13,26 +17,38 @@ const LogoutConfirmationDialog = ({ visible, onHide }) => {
     onHide();
   };
 
-  const onKeyDown = e => {
+  const onClose = () => {
+    onHide();
+  };
+
+  const onKeyDown = (e) => {
     if (e.key === 'Enter') {
-      onHide();
+      onClose();
     }
   };
 
+
+
+  if (!isAuthenticated) {
+    return <Navigate to="/" />;
+  }
   return (
     <>
       <GlobalConfirmDialogStyles overlayVisible={visible} />
-      <div className="custom-confirm-overlay" onClick={onHide} />
+      <div
+        className="custom-confirm-overlay"
+        onClick={onClose}
+      />
       <ConfirmDialog
         visible={visible}
-        onHide={onHide}
+        onHide={onClose}
         header="Log out"
         message="Do you really want to leave?"
         icon="pi pi-exclamation-triangle"
         acceptLabel="Log out"
         rejectLabel="Cancel"
         accept={onConfirm}
-        reject={onHide}
+        reject={() => {}} 
         onKeyDown={onKeyDown}
         className="custom-confirm-dialog"
         contentClassName="custom-confirm-dialog-content"
