a93ce751f2fcdd100fc05da8b72d5c393b8c0895
Serhii01071994 <126762718+Serhii01071994@users.noreply.github.com>
2024-02-05 14:46:45 +0200

    progressBar

--- src/components/ProgressBar/ProgressBar.jsx
+++ src/components/ProgressBar/ProgressBar.jsx
@@ -1,32 +1,61 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { ProgressBarStyled } from './ProgressBar.styled';
 import { ReactComponent as Icon } from './progressBar-icon.svg';
+import { useDispatch, useSelector } from 'react-redux';
+import { totalWaterAmmountSelector } from 'Store/water/waterSelectors';
+import { Slider } from '@mui/material';
+import { Modal } from 'components/Modal/Modal';
+import { AddWaterModal } from 'components/TodayWaterList/modals/AddWaterModal';
+import { EditWaterModal } from 'components/TodayWaterList/modals/EditWaterModal';
+import { selectDailyNorm } from 'Store/currentUser/currentUserSelectors';
+import {
+  toggleAddWateVisibility,
+  toggleEditWateVisibility,
+} from 'Store/modals/modalSlice';
+import { selectAddWater, selectEditWater } from 'Store/modals/modalSelector';
+import { getDailyWaterThunk } from 'Store/water/waterThunks';
 
 export const ProgressBar = () => {
-  const [progress, setProgress] = useState(0);
-  const handleButtonClick = () => {
-    setProgress(progress + 10);
-  };
-  const getColor = () => {
-    if (progress === 0) {
-      return '#d7e3ff';
-    } else {
-      return '#9ebbff';
-    }
+  const waterAmount = useSelector(totalWaterAmmountSelector);
+  const waterNorma = useSelector(selectDailyNorm);
+  const progress = (waterAmount / waterNorma) * 100;
+  console.log(`waterNorma`, waterNorma);
+console.log(`waterAmount`, waterAmount);
+  const dispatch = useDispatch();
+
+  const showAddModal = useSelector(selectAddWater);
+  const showEditModal = useSelector(selectEditWater);
+
+  const [currentEditObj] = useState(null);
+
+  const CustomSliderStyles = {
+    width: '256px',
+    padding: '0',
+    '& .MuiSlider-thumb': {
+      color: '#ffffff',
+      border: '1px solid #407bff',
+      width: '14px',
+      height: '14px',
+    },
+    '& .MuiSlider-track': {
+      color: '#9ebbff',
+      height: '8px',
+    },
+    '& .MuiSlider-rail': {
+      height: '8px',
+      color: '#9ebbff',
+    },
   };
 
   return (
     <ProgressBarStyled>
       <div className="progress-bar-block">
         <h2 className="progress-bar-title">Today</h2>
-        <div className="progress-bar">
-          <div
-            className="progress-bar-fill"
-            style={{ width: `${progress}%`, backgroundColor: getColor() }}
-          >
-            <div className="moving-dot" style={{ left: `${progress}%` }}></div>
-          </div>
-        </div>
+        <Slider
+          className="progress-bar"
+          sx={CustomSliderStyles}
+          value={progress}
+        />
         <ul className="progress-bar-lines">
           <li className="progress-bar-line"></li>
           <li className="progress-bar-line"></li>
@@ -39,7 +68,23 @@ export const ProgressBar = () => {
         </ul>
       </div>
       <div className="button-block">
-        <button onClick={handleButtonClick} className="progress-bar-button">
+        <button
+          className="progress-bar-button"
+          onClick={() => dispatch(toggleAddWateVisibility())}
+        >
+          {showAddModal && (
+            <Modal toggleModal={toggleAddWateVisibility}>
+              <AddWaterModal togleModal={toggleAddWateVisibility} />
+            </Modal>
+          )}
+          {showEditModal && (
+            <Modal toggleModal={toggleEditWateVisibility}>
+              <EditWaterModal
+                prevVal={currentEditObj}
+                togleModal={toggleEditWateVisibility}
+              />
+            </Modal>
+          )}
           <span className="svg-icon">
             <Icon />
           </span>
