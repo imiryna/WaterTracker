0c64277430ef64d7480ea3c777cd480a57e54f4d
Iryna Burlaka <i.am.blinova@gmail.com>
2024-02-07 08:40:37 +0100

    deploy fix

--- src/components/UpdatePassword/UpdatePassword.jsx
+++ src/components/UpdatePassword/UpdatePassword.jsx
@@ -1,62 +1,74 @@
-import React, {useState, useEffect} from 'react';
-import { Navigate, redirect, useLocation } from 'react-router-dom';
-import { AuthStyledForm, AuthDiv, FormName, InputDiv, StyledInput, FormButton, AuthDataError, RedirectButton } from '../SignIn/SignIn.styled';
-import { selectAuthError, selectAuthAuthenticated } from 'Store/auth/authSelector';
+import React, { useState, useEffect } from 'react';
+import { Navigate, redirect } from 'react-router-dom';
+import {
+  AuthStyledForm,
+  AuthDiv,
+  FormName,
+  InputDiv,
+  StyledInput,
+  FormButton,
+  AuthDataError,
+  RedirectButton,
+} from '../SignIn/SignIn.styled';
+import {
+  selectAuthError,
+  selectAuthAuthenticated,
+} from 'Store/auth/authSelector';
 import { useFormik } from 'formik';
 import { useDispatch, useSelector } from 'react-redux';
-import * as Yup from "yup";
+import * as Yup from 'yup';
 import Snackbar from '@mui/material/Snackbar';
 import Alert from '@mui/material/Alert';
 import { updatePasswordThunk } from 'Store/auth/authOperations';
 
-
- const validationSchema = Yup.object({
-    password: Yup
-        .string('Enter your password')
-        .min(8, 'Password should be of minimum 8 characters length')
-        .max(64, 'Password should be of maximum 64 characters length')
-        .required('Password is required'),
-    repeatPassword: Yup
-        .string('Enter your password')
-        .oneOf([Yup.ref('password'), null], 'Passwords must match')
-        .required('Password is required'),
+const validationSchema = Yup.object({
+  password: Yup.string('Enter your password')
+    .min(8, 'Password should be of minimum 8 characters length')
+    .max(64, 'Password should be of maximum 64 characters length')
+    .required('Password is required'),
+  repeatPassword: Yup.string('Enter your password')
+    .oneOf([Yup.ref('password'), null], 'Passwords must match')
+    .required('Password is required'),
 });
 
- export const UpdatePasswordForm = () => {
+export const UpdatePasswordForm = () => {
+  const dispatch = useDispatch();
+  const authError = useSelector(selectAuthError);
+  const isAuthenticated = useSelector(selectAuthAuthenticated);
 
- const dispatch = useDispatch();
- const authError = useSelector(selectAuthError);
- const isAuthenticated = useSelector(selectAuthAuthenticated);
- const location = useLocation();
+  const [openSnackbar, setOpenSnackbar] = useState(false);
 
- const [openSnackbar, setOpenSnackbar] = useState(false);
+  const formik = useFormik({
+    initialValues: {
+      password: '',
+      repeatPassword: '',
+    },
 
-   const formik = useFormik({
-     initialValues: {
-        password: '', 
-        repeatPassword: ''
-      },
-
-      validationSchema: validationSchema,
-      onSubmit: async (values) => {
-        try {
-            await dispatch(updatePasswordThunk({restoreToken, password: {password: values.password}}));
-            formik.resetForm();
-            return redirect('/signin');
-        } catch (error) {
-          setOpenSnackbar(true);
-        }
+    validationSchema: validationSchema,
+    onSubmit: async values => {
+      try {
+        await dispatch(
+          updatePasswordThunk({
+            restoreToken,
+            password: { password: values.password },
+          })
+        );
+        formik.resetForm();
+        return redirect('/signin');
+      } catch (error) {
+        setOpenSnackbar(true);
       }
-   });
+    },
+  });
 
   //  get token
   const url = window.location.href;
   const urlObject = new URL(url);
   const pathname = urlObject.search;
-  const segment = (pathname.split('='));
+  const segment = pathname.split('=');
   const restoreToken = segment[1];
 
-   useEffect(() => {
+  useEffect(() => {
     if (authError) {
       setOpenSnackbar(true);
     }
@@ -70,51 +82,55 @@ import { updatePasswordThunk } from 'Store/auth/authOperations';
     return <Navigate to="/signin" />;
   }
 
-   return (
+  return (
     <AuthDiv>
-
-     <AuthStyledForm onSubmit={formik.handleSubmit}>
+      <AuthStyledForm onSubmit={formik.handleSubmit}>
         <FormName>Reset password</FormName>
-       <InputDiv>
-            <label htmlFor="password">Enter your new password</label>
-             <StyledInput
-                id="password"
-                name="password"
-                type="password"
-                onChange={formik.handleChange}
-                value={formik.values.password}
-                placeholder='Password'
-            />
-            {formik.errors.password && formik.touched.password ? (
+        <InputDiv>
+          <label htmlFor="password">Enter your new password</label>
+          <StyledInput
+            id="password"
+            name="password"
+            type="password"
+            onChange={formik.handleChange}
+            value={formik.values.password}
+            placeholder="Password"
+          />
+          {formik.errors.password && formik.touched.password ? (
             <AuthDataError>{formik.errors.password}</AuthDataError>
           ) : null}
-       </InputDiv>
+        </InputDiv>
 
-       <InputDiv>
-            <label htmlFor="repeatPassword">Repeat your password</label>
-             <StyledInput
-                id="repeatPassword"
-                name="repeatPassword"
-                type="password"
-                onChange={formik.handleChange}
-                value={formik.values.repeatPassword}
-                placeholder='Repeat password'
-            />
-            {formik.errors.repeatPassword && formik.touched.repeatPassword ? (
-    <AuthDataError>{formik.errors.repeatPassword}</AuthDataError>
-  ) : null}
-       </InputDiv> 
+        <InputDiv>
+          <label htmlFor="repeatPassword">Repeat your password</label>
+          <StyledInput
+            id="repeatPassword"
+            name="repeatPassword"
+            type="password"
+            onChange={formik.handleChange}
+            value={formik.values.repeatPassword}
+            placeholder="Repeat password"
+          />
+          {formik.errors.repeatPassword && formik.touched.repeatPassword ? (
+            <AuthDataError>{formik.errors.repeatPassword}</AuthDataError>
+          ) : null}
+        </InputDiv>
 
-       <FormButton type="submit">Reset password</FormButton>
-       <RedirectButton to="/signin">Sign In</RedirectButton>
-     </AuthStyledForm>
-     <Snackbar open={openSnackbar} autoHideDuration={6000} onClose={handleCloseSnackbar} anchorOrigin={{ vertical: 'top', horizontal: 'right' }}>
-          <Alert onClose={handleCloseSnackbar} severity="error">
-            Fill the fields to reset your password
-          </Alert>
-        </Snackbar>
-     </AuthDiv>
-   );
- };
+        <FormButton type="submit">Reset password</FormButton>
+        <RedirectButton to="/signin">Sign In</RedirectButton>
+      </AuthStyledForm>
+      <Snackbar
+        open={openSnackbar}
+        autoHideDuration={6000}
+        onClose={handleCloseSnackbar}
+        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      >
+        <Alert onClose={handleCloseSnackbar} severity="error">
+          Fill the fields to reset your password
+        </Alert>
+      </Snackbar>
+    </AuthDiv>
+  );
+};
 
-export default UpdatePasswordForm; 
+export default UpdatePasswordForm;
